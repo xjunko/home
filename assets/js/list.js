e4f184437fc94f30be5c00a9f4d9f3457c3e00ed
FireRedz <fireredz69420@gmail.com>
2022-10-22 07:46:49 +0800

    Make site list work with index + css update

--- assets/js/list.js
+++ assets/js/list.js
@@ -1,79 +1,57 @@
 links = [
     {
-        "url": "https://junko.konno.tk",
-        "title": "Homepage",
-        "description": "The motherfucking homepage."
+        "url": "https://archlinux.org/",
+        "description": "i use arch btw",
+        "logo": "./assets/img/logo/archlinux.gif"
     },
 
     {
-        "url": "https://yuzumi.yuzuhara.tk",
-        "title": "Homepage (Alternative) (Dead)",
-        "description": "The same homepage but with a different domain."
+        "url": "https://sdf.org/",
+        "description": "SDF.org",
+        "logo": "./assets/img/logo/sdf.png"
     },
 
     {
-        "url": "#",
-        "title": "-------------------------------",
-        "description": "-------------------------------"
-    },
-
-    {
-        "url": "#TODO",
-        "title": "TODO: Add more links",
-        "description": "TODO: Add more links"
+        "url": "http://lucky-ch.com/",
+        "description": "Lucky Star",
+        "logo": "./assets/img/logo/konata.gif"
     }
 ];
 
 function generate_html_from_link_info(data) {
-    /*
-        <tr>
-                <td>
-                    <a class="link-title link" href="https://twitter.com/junkokonn0"> #LINK_NAME_HERE </a>
-                </td>
-
-                <td style="text-align:center"><a aria-label="spacer">&nbsp;&nbsp;&nbsp;</a></td>
-
-                <td>
-                    <a class="link-description"> #DESCRIPTION </a>
-                </td>
-        </tr>
-    */
-
-    // Start generating html with browser api
-    table_row = document.createElement("tr");
+    content_row = document.createElement("div")
 
     // Title
-    row_title_container = document.createElement("td");
+    row_title_container = document.createElement("div");
     row_title = document.createElement("a");
-    row_title.classList.add("link-title", "link", "underline");
+    row_title.classList.add("link-title", "link", "underline", "grid-link");
     row_title.href = data.url;
     row_title.innerText = data.title;
     row_title_container.appendChild(row_title);
 
-    // Spacer thingies
-    row_space = document.createElement("td");
-    row_space.style["text-align"] = "center";
-    row_space_text = document.createElement("a")
-    row_space_text.innerText = "   ";
-    row_space.appendChild(row_space_text);
 
-    // Description
-    row_description_container = document.createElement("td");
-    row_description = document.createElement("a");
-    row_description.classList.add("link-description");
-    row_description.innerText = data.description;
-    row_description_container.appendChild(row_description);
+    // Logo (if any)
+    if (data.logo) {
+        row_title.innerText = "";
+        row_title_logo = document.createElement("img");
+        row_title_logo.classList.add("link-title", "link", "underline", "link-logo");
+        row_title_logo.src = data.logo;
+        row_title_logo.alt = data.description;
+        row_title_logo.href = data.url;
+        row_title_logo.title = `${data.url}: ${data.description}`;
+        row_title.appendChild(row_title_logo);
+    }
 
-    // Finish
-    table_row.appendChild(row_title_container);
-    table_row.appendChild(row_space);
-    table_row.appendChild(row_description_container);
+    // Done
+    content_row.appendChild(row_title_container);
 
-    return table_row;
+    console.log(content_row);
+
+    return content_row
 }
 
 // here we go
-lists = document.getElementById("list_shit");
+lists = document.getElementById("grid_shit");
 
 for (let i = 0; i < links.length; i++) {
     lists.appendChild(generate_html_from_link_info(links[i]));
