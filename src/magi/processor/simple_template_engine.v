ed2d8ed20868505d1ffd0d18a7ab56428b463d37
xJunko <fireredz69420@gmail.com>
2024-07-17 21:33:34 +0800

    chore: Remove unused import and commented code in simple_template_engine.v

--- src/magi/processor/simple_template_engine.v
+++ src/magi/processor/simple_template_engine.v
@@ -1,7 +1,6 @@
 module processor
 
 import os
-// import x.templating.dtm
 
 // dumb fucking template engine cuz im dumb
 pub struct SimpleTemplateProcessor {
@@ -11,19 +10,14 @@ pub mut:
 
 pub fn (mut template SimpleTemplateProcessor) process(text string) string {
 	mut result := []string{}
-	// mut dtmi := &dtm.DynamicTemplateManager(unsafe { nil })
 
 	for line in text.split_into_lines() {
 		if line.trim_space().starts_with('@include') || line.contains('@include') {
-			// if isnil(dtmi) { // we only want to create the instance when we need it.
-			// 	dtmi = dtm.initialize()
-			// }
-
 			template_path := @VMODROOT + '/src/magi/templates/' +
 				line.split_nth('@include', 2)[1].replace('"', '').trim_space()
 
 			if os.exists(template_path) {
-				println('[Template] File include: ${template_path}')
+				println('[Template] File include: ${template_path} | from ${line.trim_space()}')
 
 				result << os.read_file(template_path) or { panic(err) }
 			} else {
