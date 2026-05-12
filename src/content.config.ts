f6b13cf2c33fc27188b1b006a82b66e5dc89e992
xjunko <phony@kafu.ovh>
2026-05-13 07:41:48 +0800

    content: simplify

--- src/content.config.ts
+++ src/content.config.ts
@@ -52,6 +52,11 @@ function _type_from_filename(filename: string): string {
   }
 }
 
+function _get_filename(path?: string) {
+  if (!path) return "unknown";
+  return path.split("/").filter(Boolean).pop() ?? "unknown";
+}
+
 const blog_entries = defineCollection({
   loader: glob({ base: "./src/content/blog", pattern: "**/**/*.md" }),
   schema: ({}) =>
@@ -68,42 +73,25 @@ const blog_entries = defineCollection({
 
 const channel_entries = defineCollection({
   loader: glob({ base: "./src/content/@chan", pattern: "**/*.md" }),
-  schema: ({}) =>
-    z
-      .object({
-        author: z.string(),
-        date: z.preprocess(_parse_unix_date, z.date()),
-        tags: z.preprocess(_parse_tags, z.array(z.string()).default([])),
-        thumbnail: z.string().optional(),
-      })
-      .transform((data) => ({
+  schema: z
+    .object({
+      author: z.string(),
+      date: z.preprocess(_parse_unix_date, z.date()),
+      tags: z.preprocess(_parse_tags, z.array(z.string()).default([])),
+      thumbnail: z.string().optional(),
+    })
+    .transform((data) => {
+      const filename = _get_filename(data.thumbnail);
+
+      return {
         ...data,
-        filename:
-          typeof data.thumbnail === "string"
-            ? data.thumbnail
-                .split("/")
-                .filter((p) => p.length > 0)
-                .pop() || "unknown"
-            : "unknown",
-        mimetype:
-          typeof data.thumbnail === "string"
-            ? _mime_from_filename(
-                data.thumbnail
-                  .split("/")
-                  .filter((p) => p.length > 0)
-                  .pop() || "unknown",
-              )
-            : "application/octet-stream",
-        type:
-          typeof data.thumbnail === "string"
-            ? _type_from_filename(
-                data.thumbnail
-                  .split("/")
-                  .filter((p) => p.length > 0)
-                  .pop() || "unknown",
-              )
-            : "image",
-      })),
+        filename,
+        mimetype: data.thumbnail
+          ? _mime_from_filename(filename)
+          : "application/octet-stream",
+        type: data.thumbnail ? _type_from_filename(filename) : "image",
+      };
+    }),
 });
 
 export const collections = { blog_entries, channel_entries };
