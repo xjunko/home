4a07702c6e728183f42e13d767dc4fc0be78cac4
xjunko <phony@kafu.ovh>
2026-05-13 07:16:03 +0800

    @chan: add support for videos

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,87 +1,109 @@
-import { defineCollection, type RenderResult, render, z } from "astro:content";
+import { defineCollection, type RenderResult, render } from "astro:content";
+import { z } from "astro/zod";
 import { glob } from "astro/loaders";
 
 function _parse_unix_date(val: unknown): Date {
-	if (typeof val === "string" || typeof val === "number") {
-		const num = Number(val);
-		if (!Number.isNaN(num)) {
-			return new Date(num * 1000);
-		}
-	}
+  if (typeof val === "string" || typeof val === "number") {
+    const num = Number(val);
+    if (!Number.isNaN(num)) {
+      return new Date(num * 1000);
+    }
+  }
 
-	return new Date(0);
+  return new Date(0);
 }
 
 function _parse_tags(val: unknown): string[] {
-	if (typeof val === "string") {
-		return val.split(",").map((t) => t.trim());
-	}
+  if (typeof val === "string") {
+    return val.split(",").map((t) => t.trim());
+  }
 
-	return [];
+  return [];
 }
 
 function _mime_from_filename(filename: string): string {
-	const ext = filename.toLowerCase().split(".").pop() || "";
-	switch (ext) {
-		case "png":
-			return "png";
-		case "jpg":
-		case "jpeg":
-			return "jpeg";
-		case "webp":
-			return "webp";
-		case "gif":
-			return "gif";
-		case "svg":
-			return "svg+xml";
-		default:
-			return "any";
-	}
+  const ext = filename.toLowerCase().split(".").pop() || "";
+  switch (ext) {
+    case "png":
+      return "png";
+    case "jpg":
+    case "jpeg":
+      return "jpeg";
+    case "webp":
+      return "webp";
+    case "gif":
+      return "gif";
+    case "svg":
+      return "svg+xml";
+    case "mp4":
+      return "mp4";
+    default:
+      return "any";
+  }
+}
+
+function _type_from_filename(filename: string): string {
+  const ext = filename.toLowerCase().split(".").pop() || "";
+  switch (ext) {
+    case "mp4":
+      return "video";
+    default:
+      return "image";
+  }
 }
 
 const blog_entries = defineCollection({
-	loader: glob({ base: "./src/content/blog", pattern: "**/*.md" }),
-	schema: ({}) =>
-		z.object({
-			author: z.string(),
-			date: z.preprocess(_parse_unix_date, z.date()),
-			slug: z.string(),
-			tagline: z.string(),
-			title: z.string(),
-			description: z.string(),
-			tags: z.preprocess(_parse_tags, z.array(z.string()).default([])),
-		}),
+  loader: glob({ base: "./src/content/blog", pattern: "**/**/*.md" }),
+  schema: ({}) =>
+    z.object({
+      author: z.string(),
+      date: z.preprocess(_parse_unix_date, z.date()),
+      slug: z.string(),
+      tagline: z.string(),
+      title: z.string(),
+      description: z.string(),
+      tags: z.preprocess(_parse_tags, z.array(z.string()).default([])),
+    }),
 });
 
 const channel_entries = defineCollection({
-	loader: glob({ base: "./src/content/@chan", pattern: "**/*.md" }),
-	schema: ({}) =>
-		z
-			.object({
-				author: z.string(),
-				date: z.preprocess(_parse_unix_date, z.date()),
-				tags: z.preprocess(_parse_tags, z.array(z.string()).default([])),
-				thumbnail: z.string().optional(),
-			})
-			.transform((data) => ({
-				...data,
-				filename:
-					typeof data.thumbnail === "string"
-						? data.thumbnail
-								.split("/")
-								.filter((p) => p.length > 0)
-								.pop() || "unknown"
-						: "unknown",
-				mimetype:
-					typeof data.thumbnail === "string"
-						? _mime_from_filename(
-								data.thumbnail
-									.split("/")
-									.filter((p) => p.length > 0)
-									.pop() || "unknown",
-							)
-						: "application/octet-stream",
-			})),
+  loader: glob({ base: "./src/content/@chan", pattern: "**/*.md" }),
+  schema: ({}) =>
+    z
+      .object({
+        author: z.string(),
+        date: z.preprocess(_parse_unix_date, z.date()),
+        tags: z.preprocess(_parse_tags, z.array(z.string()).default([])),
+        thumbnail: z.string().optional(),
+      })
+      .transform((data) => ({
+        ...data,
+        filename:
+          typeof data.thumbnail === "string"
+            ? data.thumbnail
+                .split("/")
+                .filter((p) => p.length > 0)
+                .pop() || "unknown"
+            : "unknown",
+        mimetype:
+          typeof data.thumbnail === "string"
+            ? _mime_from_filename(
+                data.thumbnail
+                  .split("/")
+                  .filter((p) => p.length > 0)
+                  .pop() || "unknown",
+              )
+            : "application/octet-stream",
+        type:
+          typeof data.thumbnail === "string"
+            ? _type_from_filename(
+                data.thumbnail
+                  .split("/")
+                  .filter((p) => p.length > 0)
+                  .pop() || "unknown",
+              )
+            : "image",
+      })),
 });
 
 export const collections = { blog_entries, channel_entries };
